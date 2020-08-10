e16780006148e9c0c1ea3f012a405d10b3373f57
aksige <aksige@gmail.com>
2020-08-10 12:29:13 +0400

    change: add

--- script.js
+++
@@ -1,8 +0,0 @@
-const
-	menu = document.querySelector('.navigation'),
-	noneDefault = document.querySelector('.nav-ul')
-
-
-menu.addEventListener("click",function () {
-	noneDefault.classList.toggle('none-default');
-});
